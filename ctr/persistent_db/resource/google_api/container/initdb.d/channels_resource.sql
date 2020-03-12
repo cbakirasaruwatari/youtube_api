ca45d88147ccd07cf0387f2fb1dddb274030d06e
cbakirasaruwatari <saruwatari_akira@cyberbuzz.co.jp>
2020-03-12 15:15:40 +0900

    :)

--- ctr/persistent_db/resource/google_api/container/initdb.d/channels_resource.sql
+++ ctr/persistent_db/resource/google_api/container/initdb.d/channels_resource.sql
@@ -3,3 +3,4 @@ CREATE TABLE `channels_resource` (
   `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `channels_resource_UN` (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
